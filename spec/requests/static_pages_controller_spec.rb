8108245054113b177ae8c26d3ad1e80b5343024c
Nick Robertson <nick.robertson@octanner.com>
2017-10-13 11:39:57 -0600

    add contact

--- spec/requests/static_pages_controller_spec.rb
+++ spec/requests/static_pages_controller_spec.rb
@@ -1,10 +1,14 @@
 require "rails_helper"
 
 RSpec.describe StaticPagesController, :type => :controller do
+  render_views
+    let(:title) { 'Ruby on Rails Tutorial Sample App' }
+
   describe 'GET #home' do
     it 'returns a 200 custom status code' do
       get 'home'
       expect(response.status).to eq(200)
+      expect(response.body).to include("<title>Home | #{title}</title>")
     end
   end
 
@@ -12,6 +16,7 @@ RSpec.describe StaticPagesController, :type => :controller do
     it 'returns a 200 custom status code' do
       get 'help'
       expect(response.status).to eq(200)
+      expect(response.body).to include("<title>Help | #{title}</title>")
     end
   end
 
@@ -19,6 +24,15 @@ RSpec.describe StaticPagesController, :type => :controller do
     it 'returns a 200 custom status code' do
       get 'about'
       expect(response.status).to eq(200)
+      expect(response.body).to include("<title>About | #{title}</title>")
+    end
+  end
+
+  describe 'GET #contact' do
+    it 'returns a 200 custom status code' do
+      get 'contact'
+      expect(response.status).to eq(200)
+      expect(response.body).to include("<title>Contact | #{title}</title>")
     end
   end
 end
